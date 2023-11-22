85873b6a45f74f8b2a286596857a2010d619f637
Ameer <ameerbsc2630@gmail.com>
2023-11-22 11:24:30 +0530

    home layout working header footer

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -10,12 +10,12 @@
 </head>
 
 <body class="font-sans">
-    <header class="bg-[#f4dca8] py-4 px-6 md:flex md:items-center md:justify-between">
+    <header class="bg-[#f4dca8] py-4 px-6 md:flex md:items-center md:justify-between w-full">
         <img src="{{ URL('images/logo-1an.png')}}" alt="Logo" class="w-52 h-auto cursor-pointer">
 
         <div class="flex flex-col md:flex-row  md:w-2/3 lg:w-1/2">
             <!-- Dropdown menu -->
-            <div class="md:w-25 flex">
+            <div class="md:w-25 flex items-center justify-center">
                 <select id="dropdown" class="bg-white w-full md:w-16 font-medium text-sm px-3 py-2 inline items-center">
                     <option value="" selected="selected">All</option>
                     <option value="shashvata-anna-nidhi">Shashvata Anna Nidhi</option>
@@ -32,11 +32,11 @@
                     </option>
                 </select>
 
-                <div class="md:w-72  flex mt-4 md:mt-0">
+                <div class="md:w-72 flex mt-4 md:mt-0 ">
                     <input type="text"
-                        class="border border-gray-300 py-2 px-4 md:px-16  focus:outline-none focus:border-blue-500 w-96"
+                        class="border border-gray-300  md:px-16  focus:outline-none focus:border-blue-500 w-96"
                         placeholder="I want to make puja / Homam .....">
-                    <button class="px-4 py-2 bg-amber-500  md:mt-0 w-full md:w-auto">Search</button>
+                    <button class="px-4 py-1.5 bg-amber-500  md:mt-0 w-full md:w-auto">Search</button>
                 </div>
             </div>
 
@@ -60,10 +60,10 @@
                         d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
                 </svg>
                 <span class="flex flex-col">
-                    <a class=" font-medium  text-sm block ">
+                    <a class=" font-medium  text-sm block cursor-pointer ">
                         Login
                     </a>
-                    <a class=" font-medium  text-sm block">
+                    <a class=" font-medium  text-sm block cursor-pointer">
                         Register
                     </a>
                 </span>
@@ -72,5 +72,6 @@
         </div>
 
     </header>
+   
 
     <!-- Rest of your content goes here -->
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,57 +1,101 @@
-
-    <style>
-        body {
-            font-family: 'Arial', sans-serif;
-            margin: 0;
-            padding: 0;
-        }
-
-        x-header, x-footer {
-            /* You can add styles for your custom elements here */
-            display: block;
-            background-color: #fcb800;
-            color: #fff;
-            padding: 10px;
-            
-        }
-
-        ul {
-            list-style: none;
-            padding: 0;
-            margin: 0;
-            display: flex;
-            background-color: #fcb800;
-            justify-content: space-around;
-        }
-
-        li {
-            margin: 0;
-            padding: 10px;
-            cursor: pointer;
-            font-weight: bold;
-
-        }
-
-        li:hover {
-         color: white;
-        }
-    </style>
-
-
 <x-header />
+<x-navbar />
+
+<div class="max-w-full mx-auto">
+
+    <div id="default-carousel" class="relative" data-carousel="static">
+        <div class="overflow-hidden relative h-56 sm:h-64 xl:h-80 2xl:h-96">
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                <span
+                    class="absolute top-1/2 left-1/2 text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 sm:text-3xl ">Primer
+                    Slide</span>
+                <img src="{{URL('images/temple1.jpg')}}"
+                    class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2 object-fill"
+                    alt="...">
+            </div>
+
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                <img src="{{URL('images/temple2.jpg')}}"
+                    class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
+            </div>
+
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                <img src="{{URL('images/temple3.jpg')}}"
+                    class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
+            </div>
+
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                <img src="{{URL('images/temple4.jpg')}}"
+                    class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
+            </div>
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                <img src="{{URL('images/temple5.webp')}}"
+                    class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
+            </div>
+
+        </div>
+
+
+        <div class="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
+            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 1"
+                data-carousel-slide-to="0"></button>
+            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2"
+                data-carousel-slide-to="1"></button>
+            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3"
+                data-carousel-slide-to="2"></button>
+            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3"
+                data-carousel-slide-to="2"></button>
+            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3"
+                data-carousel-slide-to="2"></button>
+        </div>
+
+        <button type="button"
+            class="flex absolute top-0 left-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none"
+            data-carousel-prev>
+            <span
+                class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30  group-focus:ring-4 group-focus:ring-white  group-focus:outline-none">
+                <svg class="w-5 h-5 text-white sm:w-6 sm:h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
+                </svg>
+                <span class="hidden">Anterior</span>
+            </span>
+        </button>
+        <button type="button"
+            class="flex absolute top-0 right-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none"
+            data-carousel-next>
+            <span
+                class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 0 group-focus:ring-4 group-focus:ring-white  group-focus:outline-none">
+                <svg class="w-5 h-5 text-white sm:w-6 sm:h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
+                </svg>
+                <span class="hidden">Siguiente</span>
+            </span>
+        </button>
+    </div>
+
+
+    <section class="flex">
+        <div>
+        <h2 class="p-4 bg-red-200 text-3xl">Upcoming Events</h2>
+        </div>
+        <div>
+            <div>
+            <img src="{{URL('images/event image 1.jpeg')}}">
+
+
+            </div>
+            <div>
 
 
 
-<ul>
-    <li>HOME</li>
-    <li>SEVAS/SERVICES</li>
-    <li>ABOUT US</li>
-    <li>ANNUAL EVENTS</li>
-    <li>DONATIONS</li>
-    <li>PHOTOS</li>
-    <li>SONGS</li>
-    <li>VIDEOS</li>
-    <li>CONTACT US</li>
-</ul>
+            </div>
+        </div>
+        
+    </section>
+
+    <script src="https://unpkg.com/flowbite@1.4.0/dist/flowbite.js"></script>
+</div>
 
 <x-footer />
